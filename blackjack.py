f4f9aa2e9fe0cdd346e2a726e56199ceca1427db
Josh Navarro <joshua.navarro@sjsu.edu>
2019-02-24 21:38:03 -0800

    added deck class

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,8 @@
 import random
 
-suit = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-rank = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
-value = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10,
+suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10,
  'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 playing = True
@@ -23,5 +23,53 @@ class Deck():
     '''
     A class to hold 52 card objects to create a standard deck of cards
     '''
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
 
-    def __init__
+    def __str__(self):
+        return ','.join(map(str,self.deck))
+
+    def shuffle(self):
+        '''
+        Shuffle the current deck of cards
+        '''
+        random.shuffle(self.deck)
+
+    def deal(self):
+        '''
+        Deal one card out from top of deck (index 0)
+        '''
+        return self.deck.pop(0)
+
+class Hand():
+    '''
+    A class to hold each player's cards in hand
+    '''
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        '''
+        Add a single card to the players hand
+        '''
+        pass
+
+    def adjust_for_ace(self):
+        pass
+
+def main():
+    deck = Deck()
+    print(deck)
+    deck.shuffle()
+    print("\n\nDeck has been shuffled\n")
+    print(deck)
+    print(deck.deal())
+    print(deck)
+
+if __name__ == '__main__':
+	main()
